90e406c358fdb4796d229cebf1dba34a95483f31
ray_tsai <raytsai0212@gmail.com>
2023-10-29 22:14:10 +0800

    feat:  search page  test code

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,89 +1,75 @@
 <template>
-  <div class="">
-    search
-    <div>
-      <router-link to="/">
-        home
-      </router-link>
-      <button @click="updatePageIndex">
-        updatePageIndex
-      </button>
-    </div>
-    <router-link :to="{ name: 'fake', query: { un: undefined } }">
-      to fake with undefined query
-    </router-link>
-
-    <div>
-      {{ queryData }}
-
-      <pre>
+  <div class="page">
+    <h3>current page: {{ route.name }}</h3>
+    <div class="page-view">
+      <div class="test-operate">
+        <div class="query-value">
+          <pre>
         {{ beautifyQueryData }}
       </pre>
-    </div>
+        </div>
+      </div>
+      <div class="val-watch">
+        <n-grid :span="24" :x-gap="24">
+          <n-form-item-gi :span="24" label="Input" path="inputValue">
+            <n-input v-model:value="queryData.inputValue" />
+          </n-form-item-gi>
 
+          <n-form-item-gi :span="24" label="Select" path="selectValue">
+            <n-select v-model:value="queryData.selectValue" :options="generalOptions" />
+          </n-form-item-gi>
+          <n-form-item-gi :span="24" label="Radio" path="language">
+            <n-radio-group v-model:value="queryData.radioValue">
+              <n-space>
+                <n-radio label="English" value="English" />
+                <n-radio label="Chinese" value="Chinese" />
+              </n-space>
+            </n-radio-group>
+          </n-form-item-gi>
+          <n-form-item-gi :span="24" label="Switch" path="switchValue">
+            <n-switch v-model:value="queryData.switchValue" />
+          </n-form-item-gi>
+        </n-grid>
+      </div>
+    </div>
     <div>
-      <n-grid :span="24" :x-gap="24">
-        <n-form-item-gi :span="12" label="Input" path="inputValue">
-          <n-input v-model:value="queryData.inputValue" placeholder="Input" />
-        </n-form-item-gi>
-        <n-form-item-gi :span="12" label="Input" path="nullValue">
-          <n-input v-model:value="queryData.nullValue" placeholder="Input" />
-        </n-form-item-gi>
+      <div class="pi-test">
+        <h3>test pageIndex change</h3>
+        <p>if only want to update specific value, use `updateQueryData` method</p>
+        <button class="pi-test-btn" @click="changePageIndex(1)">
+          pageIndex = 1
+        </button>
+        <button class="pi-test-btn" @click="changePageIndex(2)">
+          pageIndex = 2
+        </button>
+        <button class="pi-test-btn" @click="changePageIndex(3)">
+          pageIndex = 3
+        </button>
+      </div>
 
-        <n-form-item-gi :span="12" label="Select" path="selectValue">
-          <n-select
-            v-model:value="queryData.selectValue"
-            placeholder="Select"
-            :options="generalOptions"
-          />
-        </n-form-item-gi>
-        <n-form-item-gi :span="12" label="Radio" path="language">
-          <n-radio-group v-model:value="queryData.language">
-            <n-space>
-              <n-radio label="English" value="English" />
-              <n-radio label="Chinese" value="Chinese" />
-            </n-space>
-          </n-radio-group>
-        </n-form-item-gi>
-        <n-form-item-gi :span="12" label="Switch" path="switchValue">
-          <n-switch v-model:value="queryData.switchValue" />
-        </n-form-item-gi>
-      </n-grid>
+      <div class="val-test">
+        <h3>test change page with specific values</h3>
+        <p>since only change page with specifc values, other query value will fallback to default</p>
+        <button class="val-test-btn" @click="pushToSameRoute({ inputValue: 'apple' })">
+          inputValue = 'apple'
+        </button>
+        <button class="val-test-btn" @click="pushToSameRoute({ inputValue: 'banana', selectValue: 'orange' })">
+          inputValue = 'banana', selectValue = 'orange'
+        </button>
+        <button class="val-test-btn" @click="pushToSameRoute({ radioValue: 'Chinese', switchValue: false })">
+          radioValue: 'Chinese', switchValue: false
+        </button>
+      </div>
     </div>
-
-    <div>
-      <button @click="changePageIndex(1)">
-        1
-      </button>
-      <button @click="changePageIndex(2)">
-        2
-      </button>
-      <button @click="changePageIndex(3)">
-        3
-      </button>
-    </div>
-
-    <footer class="footer">
-      footer
-
-      <button @click="pushToSameRoute(1)">
-        push A As 1
-      </button>
-      <button @click="pushToSameRoute(2)">
-        push A As 2
-      </button>
-      <button @click="pushToSameRoute(3)">
-        push A As 3
-      </button>
-    </footer>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import useSyncQuery from '@/composables/useQuerySyncData';
 import type { Rules } from '@/shared/types';
 
+const route = useRoute();
 const opts = ['apple', 'banana', 'orange'];
 const langOpts = ['English', 'Chinese'];
 const generalOptions = opts.map(
@@ -104,7 +90,7 @@ const defaultQueryData = {
   },
   pageIndex: 1,
   switchValue: true,
-  language: 'English',
+  radioValue: 'English',
   nullValue: null,
 };
 
@@ -146,33 +132,80 @@ const {
 } = useSyncQuery(defaultQueryData, rules, { queryChangeCallback });
 
 const beautifyQueryData = computed(() => {
-  return JSON.stringify(toRaw(queryData.value), null, '\t');
+  return JSON.stringify(toRaw(queryData.value), null, 2);
 });
 
 const router = useRouter();
-
-const updatePageIndex = () => {
-  updateQueryData({ pageIndex: 1 });
-};
 
 const changePageIndex = (num: number) => {
   updateQueryData({ pageIndex: num });
 };
 
-const pushToSameRoute = (num: number) => {
+const pushToSameRoute = (query: any) => {
   router.push({
     name: 'search',
-    query: {
-      a: num,
-    },
+    query,
   });
 };
 </script>
 
 <style scoped>
-.footer {
+.page {
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+
+}
+
+.page-view {
+  display: flex;
+
+}
+
+.test-operate {
+  flex: 1;
+}
+
+.val-watch {
+  flex: 1;
+  margin-left: 40px;
+}
+
+.val-test {
+  /* display: flex; */
+  /* flex-direction: column; */
+}
+
+.query-value {
   background-color: #ccc;
+  box-sizing: border-box;
+  padding: 10px;
+
+  pre {
+    margin: 0
+  }
+}
+
+.pi-test-btn {
+  border: 0;
+  outline: 0;
+  padding: 10px;
+  margin-right: 10px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.val-test-btn {
+  border: 0;
+  outline: 0;
+  padding: 10px;
+  margin-right: 10px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+button:hover {
+  opacity: 0.8;
 }
 </style>
-@/composables/useSyncQuery
-@/composables/useQuerySyncData
